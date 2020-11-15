40fe09a0a385df38cadc91d8c50ced3a0af7a881
Joe Hawkins <50604799+JosephAHawkins@users.noreply.github.com>
2020-11-14 23:33:23 -0500

    populate default initial values
    populate default initial values

--- SandwichFX/src/application/PrimaryController.java
+++ SandwichFX/src/application/PrimaryController.java
@@ -2,9 +2,13 @@ package application;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
@@ -14,23 +18,87 @@ public class PrimaryController {
 	ObservableList<String> ingredients = FXCollections
 			.observableArrayList();
 	
-    @FXML
-    private ComboBox <String> sandwichTypeBox;
+	ObservableList<String> addOnToppings = FXCollections
+			.observableArrayList();
+	
+	Sandwich sandwich;
+   
+	@FXML
+    private ComboBox <String> sandwichType;
 	
     @FXML
     private ListView <String> ingredientListView;
     
+    @FXML
+    private ListView<String> extraIngredients;
+
+    @FXML
+    private ListView<String> addedIngredients;
+    
+    @FXML
+    private ImageView sandwichPic;
+    
+    @FXML
+    private TextArea priceTextArea;
+
+
+    @FXML
+    void newSandwichSelected(ActionEvent event) {
+    	//
+    	String selection = sandwichType.getValue();
+    	if(selection.equals("Chicken")) {
+    		sandwich = new Chicken();
+    	}
+    	if(selection.equals("Fish")) {
+    		sandwich = new Fish();
+    	}
+    	if(selection.equals("Beef")) {
+    		sandwich = new Beef();
+    	}
+
+    	priceTextArea.setText("$" + String.valueOf((sandwich.price())));
+    	//change priceTextArea based on sandwich selected
+    	
+    }
+    
+    /**
+     * Adjusts the price of a sandwich based on the current toppings.
+     */
+    void adjustPrice() {
+    	
+    }
+    
+    @FXML
+    void AddIngredients(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearIngredients(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeIngredients(ActionEvent event) {
+
+    }
+    
 	@FXML
 	public void initialize() {
-		sandwichTypeBox.setValue("Chicken");
-		sandwichTypeBox.setItems(sandwiches);
+		sandwichType.setValue("Chicken");
+		sandwichType.setItems(sandwiches);
+		sandwich = new Chicken();
+    	priceTextArea.setText("$" + String.valueOf((sandwich.price())));
 		
-		
-		//ingredientListView = new ListView<String>(ingredients);
 		ingredients.addAll("Fried Chicken",
 				"Spicy Sauce",
 				"Pickles");
 		ingredientListView.getItems().addAll(ingredients);
+
+		addOnToppings.addAll("Lettuce", "Tomatoes", "Onion", "Bacon",
+					"Mushrooms", "Spinach", "Pickles", "Provolone", "American", "Swiss");
+		
+		//extraIngredients.getItems().addAll(addOnToppings);
 	}
 	
 	public void loadIngredients() {
